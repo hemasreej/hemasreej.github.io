abfda76a164ff8fe6e73d954192b81204fef1933
J.Hemasree <92966088+hemasreej@users.noreply.github.com>
2023-06-05 22:28:35 +0530

    chang

---
+++ usercard2.js
@@ -0,0 +1,29 @@
+let maheshbabu = {
+    imgurl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQbZpRNiXIlAEtQCXDJ3ehxXhjdGiAWKM2IKt_OTlHALA&usqp=CAU&ec=48665701", 
+    name: "Mahesh Babu",
+    Description: "Actor"
+}
+let maheshyounginfo = {
+    imgurl = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQbZpRNiXIlAEtQCXDJ3ehxXhjdGiAWKM2IKt_OTlHALA&usqp=CAU&ec=48665701",
+    name: "maithili",
+    Description: "Actor"
+
+
+
+}
+let ismahesh = true;
+let displayobject;
+let flipdata = function(){
+    if (ismahesh == true){
+        displayobject = maithili;
+        ismahesh = false;
+    }
+    else{
+        displayobject = maheshbabu;
+        ismahesh = true;
+    }
+    document.getElementById("maiimg").src= displayobject.imgurl;
+    document.getElementById("maithili").innerHTML = displayobject.name;
+    document.getElementById("descri").innerHTML = displayobject.Description;
+
+}
