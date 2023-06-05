ef22b4f9200d4945f3aa1cc4374206671bbe9bc3
J.Hemasree <92966088+hemasreej@users.noreply.github.com>
2023-06-05 23:00:06 +0530

    cj

--- usercard2.js
+++ usercard2.js
@@ -1,10 +1,10 @@
 let maheshbabu = {
-    imgurl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQbZpRNiXIlAEtQCXDJ3ehxXhjdGiAWKM2IKt_OTlHALA&usqp=CAU&ec=48665701", 
+    imgurl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSyHsCqSJQZ7FeKG9zr6KHJB36TJ4JyTCsfTQ&usqp=CAU", 
     name: "Mahesh Babu",
     Description: "Actor"
 }
 let maheshyounginfo = {
-    imgurl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQbZpRNiXIlAEtQCXDJ3ehxXhjdGiAWKM2IKt_OTlHALA&usqp=CAU&ec=48665701",
+    imgurl: "",
     name: "maithili",
     Description: "Actor"
 
