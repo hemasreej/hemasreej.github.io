513f9364ceee5e0a972697eb46de37de71717d7a
J.Hemasree <92966088+hemasreej@users.noreply.github.com>
2023-06-07 20:49:55 +0530

    changes made

--- usercard2.js
+++ usercard2.js
@@ -1,12 +1,12 @@
 let maheshbabu = {
     imgurl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSyHsCqSJQZ7FeKG9zr6KHJB36TJ4JyTCsfTQ&usqp=CAU", 
-    name: "Mahesh Babu",
-    Description: "Actor"
+    name: "Maithili Thakur",
+    Description: "Indian playback singer"
 }
 let maheshyounginfo = {
     imgurl: "https://wikibio.in/wp-content/uploads/2019/03/Maithili-Thakur.jpg",
     name: "maithili",
-    Description: "Actor"
+    Description: "Indian Playback Singer"
 
 
 
