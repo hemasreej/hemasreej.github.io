db290d5ebc68e240e0b16fed4d1165eb1eb9aa15
J.Hemasree <92966088+hemasreej@users.noreply.github.com>
2023-06-13 21:25:52 +0530

    changes updated

---
+++ usercard3.js
@@ -0,0 +1,29 @@
+let maheshbabu = {
+    imgurl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSyHsCqSJQZ7FeKG9zr6KHJB36TJ4JyTCsfTQ&usqp=CAU", 
+    name: "Maithili Thakur",
+    Description: "Indian playback singer"
+}
+let maheshyounginfo = {
+    imgurl: "https://wikibio.in/wp-content/uploads/2019/03/Maithili-Thakur.jpg",
+    name: "maithili",
+    Description: "Indian Playback Singer"
+
+
+
+}
+let ismahesh = true;
+let displayobject;
+let flipdata = function(){
+    if (ismahesh == true){
+        displayobject = maheshyounginfo;
+        ismahesh = false;
+    }
+    else{
+        displayobject = maheshbabu;
+        ismahesh = true;
+    }
+    document.getElementById("maiimg").src= displayobject.imgurl;
+    document.getElementById("maithili").innerHTML = displayobject.name;
+    document.getElementById("descri").innerHTML = displayobject.Description;
+
+}
